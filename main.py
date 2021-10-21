452d8452e3b8dadaa69b39c062aadbc66f48bf7d
tugsag <53309845+tugsag@users.noreply.github.com>
2021-10-21 14:29:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sklearn.linear_model import LinearRegression, Lasso, ElasticNet
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
+from tensorflow import keras
+from tensorflow.keras import layers
 
 prices = pd.read_csv('gas.csv')
 prices = prices[['Date', 'A1', 'R1', 'M1', 'P1']]
@@ -17,7 +19,7 @@ prices['week'] = prices.index
 # print(prices.tail())
 
 x = prices[['week']].values
-y = prices[['P1']].values
+y = prices[['A1']].values
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
@@ -26,6 +28,44 @@ last = datetime.strptime(prices['Date'].iloc[-1], '%m/%d/%Y')
 week_now = int(ceil((datetime.today() - last).days/7))
 print(week_now)
 
+def plot_loss(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.ylabel('error')
+    plt.show()
+
+def plot_preds(y_test, y_pred):
+    plt.plot(y_test, label='true')
+    plt.plot(y_pred, label='pred')
+    plt.legend()
+    plt.show()
+
+def past_features(type):
+    x_train = []
+    y_train = []
+    for i in range(16,len(x)):
+        x_train.append(y[i-16:i])
+        y_train.append(y[i])
+    return np.array(x_train), np.array(y_train)
+
+def dl_regression():
+    # custom data analysis
+    x_cust, y_cust = past_features('A1')
+    x_train, y_train = x_cust[:int(len(x_cust)*0.85)], y_cust[:int(len(y_cust)*0.85)]
+    x_test, y_test = x_cust[int(len(x_cust)*0.85):], y_cust[int(len(y_cust)*0.85):]
+    model = keras.Sequential([layers.Dense(64, activation='relu'), layers.Dense(64, activation='relu'), layers.Dense(1)])
+    model.compile(loss='mean_absolute_error', optimizer=keras.optimizers.Adam(0.001))
+    history = model.fit(x_train, y_train, validation_split=0.2, epochs=100, verbose=0)
+    plot_loss(history)
+    losses = history.history['loss']
+    print('Avg loss: ', sum(losses)/len(losses))
+    y_pred = model.predict(x_test)
+    # print(y_pred.mean(axis=1).shape)
+    # print(y_test.shape)
+    print('Test set: ', mean_squared_error(y_test, y_pred.mean(axis=1)))
+   
+    # plot_preds(y_test, y_pred)
+    
 
 def eval_models():
     models = []
@@ -36,11 +76,17 @@ def eval_models():
     models.append(('eln', ElasticNet()))
 
     results = []
+    best = 0
+    best_n = ''
     for name, model in models:
         kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-        res = cross_val_score(model, x_train, y_train, cv=kfold, scoring='r2')
+        res = cross_val_score(model, x_train, y_train, cv=kfold, scoring='neg_mean_absolute_error')
         results.append((name, res))
+        if best < res.mean():
+            best = res.mean()
+            best_n = name
         print('{} : {}'.format(name, res.mean()))
+    return best_n, best
 
 def use_best():
     dtr = DecisionTreeRegressor()
@@ -55,5 +101,6 @@ def use_best():
     plt.plot(next_week, pred, 'ro')
     plt.show()
 
-# eval_models()
-use_best()
+dl_regression()
+# best_model, best_score = eval_models()
+# use_best()
